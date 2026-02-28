f3a85ad97d9a12c9410893db305bf5e0efc090ca
Obito-git <antonmyroshnychenko@gmail.com>
2026-02-28 17:36:21 +0100

    jasm: add error codes for few errors. replace Diagnostic trait by a struct

--- src/jasm_asm/flags.rs
+++ src/jasm_asm/flags.rs
@@ -8,4 +8,32 @@ impl ClassFlags {
     pub fn set_final(&mut self) {
         self.0 |= 0x0010;
     }
+
+    pub fn set_super(&mut self) {
+        self.0 |= 0x0020;
+    }
+
+    pub fn set_interface(&mut self) {
+        self.0 |= 0x0200;
+    }
+
+    pub fn set_abstract(&mut self) {
+        self.0 |= 0x0400;
+    }
+
+    pub fn set_synthetic(&mut self) {
+        self.0 |= 0x1000;
+    }
+
+    pub fn set_annotation(&mut self) {
+        self.0 |= 0x2000;
+    }
+
+    pub fn set_enum(&mut self) {
+        self.0 |= 0x4000;
+    }
+
+    pub fn set_module(&mut self) {
+        self.0 |= 0x8000;
+    }
 }
